23e1e3d1b05f34f6fe2ecd63c476659e4b4902bd
Maciej <maciej.wlodek@stonybrook.edu>
2020-03-12 20:54:26 -0400

    update gitignore

---
+++ c4me-server/src/main/java/com/c4me/server/config/JpaConfig.java
@@ -0,0 +1,39 @@
+package com.c4me.server.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+/**
+ * @Description:
+ * @Author: Siyong Liu
+ * @CreateDate: 2-20-2020
+ */
+@Configuration
+public class JpaConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("siyoliu");
+        dataSource.setPassword("112381853");
+        dataSource.setUrl("jdbc:mysql://mysql3.cs.stonybrook.edu:3306/siyoliu");
+        return dataSource;
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.c4me.server.entities");
+        factory.afterPropertiesSet();
+        factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        return factory;
+    }
+}

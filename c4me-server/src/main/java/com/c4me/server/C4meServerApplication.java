09ef29c78d540868110d4e415e82ecc22dadd02f
Siyong Liu <liusy920@gmail.com>
2020-04-04 15:30:35 +0800

    remove eureka

--- c4me-server/src/main/java/com/c4me/server/C4meServerApplication.java
+++ c4me-server/src/main/java/com/c4me/server/C4meServerApplication.java
@@ -2,10 +2,8 @@ package com.c4me.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.c4me.server.core.*.repository"})
 public class C4meServerApplication {

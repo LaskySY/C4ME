ce6e82bc3f1072940310324ec5d74dcc59bc8d49
Yousef Khan <yousef.khan@stonybrook.edu>
2020-04-12 20:26:59 -0400

    edited college recommender score calculation

--- src/main/java/com/c4me/server/core/profile/service/CollegeRecommenderServiceImpl.java
+++ src/main/java/com/c4me/server/core/profile/service/CollegeRecommenderServiceImpl.java
@@ -28,6 +28,24 @@ public class CollegeRecommenderServiceImpl {
     return (attr - attr_min)/(attr_max - attr_min);
   }
 
+  public double computeZScore(Integer studentScore, Integer college25, Integer college75) {
+    if(studentScore == null || college25 == null || college75 == null) {
+      return 0;
+    }
+    double mean = (college75 - college25)/(2);
+    double sigma = (college75 - college25)/(1.348);
+    return Math.abs((studentScore - mean)/(sigma));
+  }
+
+  public double computeZScoreActComp(Integer studentScore, Integer collegeScore) {
+    if(studentScore == null || collegeScore == null) {
+      return 0;
+    }
+    double mean = collegeScore;
+    double sigma = (collegeScore)/(1.348);
+    return Math.abs((studentScore - mean)/(sigma));
+  }
+
 
 
 
@@ -37,13 +55,13 @@ public class CollegeRecommenderServiceImpl {
     ProfileEntity thisUser = profileRepository.findByUsername(username);
 
     //student z score
-    float satMath = 0;
-    float satEbrw = 0;
-    float actMath = 0;
-    float actEng = 0;
-    float actRead = 0;
-    float actSci = 0;
-    float actComp = 0;
+    double satMath = 0;
+    double satEbrw = 0;
+    double actMath = 0;
+    double actEng = 0;
+    double actRead = 0;
+    double actSci = 0;
+    double actComp = 0;
 
     if(thisUser.getSatMath() != null) {
       satMath = normalize(thisUser.getSatMath(), 200, 800);
@@ -73,7 +91,7 @@ public class CollegeRecommenderServiceImpl {
       actComp = normalize(thisUser.getActComposite(), 1, 36);
     }
 
-    float studentZScore = (satMath + satEbrw + actMath + actEng + actRead + actSci + actComp)/7;
+    double studentZScore = (satMath + satEbrw + actMath + actEng + actRead + actSci + actComp)/7;
 
     List<CollegeEntity> collegeEntities = null;
     for(String c: colleges){
@@ -86,7 +104,17 @@ public class CollegeRecommenderServiceImpl {
 
     for(CollegeEntity c : collegeEntities){
 
-      dataset.put(c.getName(), String.valueOf(Math.random()));
+      double mathZ = computeZScore(thisUser.getSatMath(), c.getSatMath25(), c.getSatMath75());
+      double ebrwZ = computeZScore(thisUser.getSatEbrw(), c.getSatEbrw25(), c.getSatEbrw75());
+      double actMZ = computeZScore(thisUser.getActMath(), c.getActMath25(), c.getActMath75());
+      double actEZ = computeZScore(thisUser.getActEnglish(), c.getActEnglish25(), c.getActEnglish75());
+      double actRZ = computeZScore(thisUser.getActReading(), c.getActReading25(), c.getActReading75());
+      double actSZ = computeZScore(thisUser.getActScience(), c.getActScience25(), c.getActScience75());
+      double actCompZ = computeZScoreActComp(thisUser.getActComposite(), c.getActComposite());
+
+      double collegeZScore = (mathZ + ebrwZ + actMZ + actEZ + actRZ + actSZ + actCompZ)/7;
+
+      dataset.put(c.getName(), String.valueOf(collegeZScore));
 
     }
 

a771fb4a9af179026905d15a6ce080004959ba31
Maciej <maciej.wlodek@stonybrook.edu>
2020-04-17 14:09:23 -0400

    change getmajorname mapping

--- src/main/java/com/c4me/server/core/profile/controller/GetAllMajorNamesController.java
+++ src/main/java/com/c4me/server/core/profile/controller/GetAllMajorNamesController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
  */
 
 @RestController
-@RequestMapping("/getAllMajorNames")
+@RequestMapping("/collegeSearch/getmajor")
 public class GetAllMajorNamesController {
 
     @Autowired
